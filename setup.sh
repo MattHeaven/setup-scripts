258793bc3b828ac63d5755b33226ff533e6bbcc0
MattHeaven <126955504+MattHeaven@users.noreply.github.com>
2024-06-26 11:19:05 +0300

    Setup Script for Raspberry

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt install python3-pip
+sudo apt install git
+sudo apt install command-not-found
+sudo apt install python3-rpi.gpio
+sudo apt install ssmtp -y
+sudo apt install gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
+sudo apt install ffmpeg -y
+sudo apt install libavcodec-extra -y
+sudo apt install nodejs
+sudo apt install npm
+sudo update-command-not-found
+sudo apt-get upgrade
+sudo apt full-upgrade
+sudo reboot
